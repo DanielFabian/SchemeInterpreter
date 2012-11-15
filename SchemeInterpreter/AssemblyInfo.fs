1283b782a4a791f0d2ccca036baeceae76d4ad11
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-11-15 17:53:05 +0100

    Added the SchemeInterpreter project

---
+++ SchemeInterpreter/AssemblyInfo.fs
@@ -0,0 +1,22 @@
+module SchemeInterpreter.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+
+[<assembly: AssemblyTitle("SchemeInterpreter")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("Integral Information Technologie GmbH")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Integral IT GmbH")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
