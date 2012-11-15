b8582c1ce60e6520f65c08ddd398cc6dc8e4f485
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-11-15 21:30:41 +0100

    Added lexer compilation as a build step

--- SchemeInterpreter/Program.fs
+++ SchemeInterpreter/Program.fs
@@ -1,11 +1,31 @@
 module Program
 
 open System
+open System.IO
+open System.Text
 
 let someFunction x y = x + y
 
 [<EntryPoint>]
 let main args = 
-    Console.WriteLine("Hello world!")
+    if args.Length <= 2 then
+        let exe = Path.GetFileName args.[0]
+        eprintfn "Usage: %s dir pattern" exe
+        exit 1
+    let directory = args.[1]
+    let pattern = args.[2]
+    
+    for fileName in Directory.GetFiles (directory, pattern) do
+        use inputReader = File.OpenText fileName
+        
+        let lexBuffer = Lexing.LexBuffer<_>.FromTextReader inputReader
+        
+        let outputFile = Path.ChangeExtension (fileName, "html")
+        use outputWriter = new StreamWriter (outputFile) :> TextWriter
+        
+        fprintfn outputWriter "<html>\n<head></head>\n<pre>"
+        
+        Text2HtmlLex.convertHtml outputWriter lexBuffer
+        
+        fprintfn outputWriter "</pre>\n</html>\n"
     0
-
