a04af0dc974838e7574f394f6446a6efa28a5fcf
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-11-16 12:08:20 +0100

    Worked through the Kitty example in professional F#. Added parser and lexer for Kitty

--- SchemeInterpreter/Program.fs
+++ SchemeInterpreter/Program.fs
@@ -4,76 +4,25 @@ open System
 open System.IO
 open System.Text
 open Lexer
-open Microsoft.FSharp.Text.Lexing
+open Parser
 
-type Term = 
-    | Term  of int * string * int
-    | Const of int
-    
-type Polynomial = Term list
-type TokenStream = LazyList<token * Position * Position>
-
-let tryToken = function
-    | LazyList.Cons ((tok, startPos, endPos), rest) -> Some (tok, rest)
-    | _ -> None
-
-let parseIndex src = 
-    match tryToken src with
-    | Some (HAT, src) -> 
-        match tryToken src with
-        | Some (INT exp, src) -> exp, src
-        | _ -> failwith "expected an integer after '^'"
-    | _ -> 1, src
-
-let parseTerm src =
-    match tryToken src with
-    | Some (INT num, src) ->
-        match tryToken src with
-        | Some (ID id, src) ->
-            let idx, src = parseIndex src
-            Term (num, id, idx), src
-        | _ -> Const num, src
-    | Some (ID id, src) ->
-        let idx, src = parseIndex src
-        Term (1, id, idx), src
-    | _ -> failwith "end of token steam in term"
-
-let negateTerm = function
-    | Term (fac, var, exp) -> Term (-fac, var, exp)
-    | Const c -> Const -c
-
-let rec parsePolynomial src = 
-    let t1, src = parseTerm src
-    match tryToken src with
-    | Some (PLUS, src) ->
-        let p2, src = parsePolynomial src
-        (t1 :: p2), src
-    | Some (MINUS, src) ->
-        let p2, src = parsePolynomial src
-        match p2 with
-        | t2 :: rest -> (t1 :: negateTerm t2 :: rest), src
-        | _ -> failwith "no term after a -"
-    | _ -> [t1], src
-    
-let getTokenStream inp =
-    seq {
-        let lexbuf = LexBuffer<_>.FromString inp
-        while not lexbuf.IsPastEndOfStream do
-            match Lexer.token lexbuf with
-            | EOF -> yield! []
-            | token -> yield (token, lexbuf.StartPos, lexbuf.EndPos) }
-    |> LazyList.ofSeq    
-
-let parse input = 
-    let src = getTokenStream input
-    let result, src = parsePolynomial src
-    match tryToken src with
-    | Some _ -> failwith "unexpected input at end of token stream!"
-    | None -> result
+let parseText text = 
+    let lexbuf = Lexing.LexBuffer<_>.FromString text
+    try
+        start token lexbuf
+    with e ->
+        let pos = lexbuf.EndPos
+        failwithf "Error near line %d, character %d\n" pos.Line pos.Column    
 
 [<EntryPoint>]
 let main _ = 
 
-    let res = parse "x^5 - 2x^3 + 20"
-    printf "%A" res      
+    let sample = @"counter := 100; accum := 0;
+                 while counter do
+                 begin
+                     counter := counter - 1;
+                     accum := accum + counter
+                 end;
+                 print accum"
+    //start "x^5 - 2x^3 + 20"
     0
