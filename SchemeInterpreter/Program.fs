6ade155d4b4f205f62d8864dcc874c8b1eb419f6
Daniel Fabian <daniel.fabian@integral-it.ch>
2012-11-18 12:30:34 +0100

    Adjusted the mono-specific build order, so that the in-session build order works right

--- SchemeInterpreter/Program.fs
+++ SchemeInterpreter/Program.fs
@@ -4,29 +4,35 @@ open System
 open System.IO
 open System.Text
 
+open Ast
+open Parser
+open Lexer
 open Evaluator
 
 
-//let parseText text = 
-//    let lexbuf = Lexing.LexBuffer<_>.FromString text
-//    try
-//        Choice1Of2 <| head <| start token lexbuf
-//    with e ->
-//        let pos = lexbuf.EndPos
-//        Choice2Of2 <| ParserError <| sprintf "Error near line %d, character %d\n" pos.Line pos.Column    
+let parseText text = 
+    let lexbuf = Lexing.LexBuffer<_>.FromString text
+    try
+        match start token lexbuf with
+        | Prog (head::tail) -> Choice1Of2 head
+        | _ -> failwith "Empty program"
+    with e ->
+        let pos = lexbuf.EndPos
+        let message = sprintf "Error near line %d, character %d\n" pos.Line pos.Column
+        Choice2Of2 <| ParserError message
 
 [<EntryPoint>]
 let main args = 
-//    let evaluated = choice {
-//        let! ast = parseText args.[0]
-//        let! evaluated = eval <| ast
-//        return evaluated
-//    }
-//    
-//    let message = 
-//        match evaluated with
-//        | Choice1Of2 result -> showVal result
-//        | Choice2Of2 error -> showError error 
-//    
-//    Console.WriteLine message
+    let evaluated = choice {
+        let! ast = parseText args.[0]
+        let! evaluated = eval <| ast
+        return evaluated
+    }
+    
+    let message = 
+        match evaluated with
+        | Choice1Of2 result -> showVal result
+        | Choice2Of2 error -> showError error 
+    
+    Console.WriteLine message
     0
